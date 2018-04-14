20b0d8d21ce1554edd2f0f2c1c03fa113067e8d4
Moon Bongki <bongki81@gmail.com>
2018-04-15 01:15:07 +0900

    Initial version of Virtual Murim

---
+++ Division.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Division {
+    private String name;    // Murim Party name, Huasan, Mudang, etc..
+    private List<PrivateCharacter> members;
+
+
+}
---
+++ Group.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public class Group {
+    private List<Division> divisions;
+}
